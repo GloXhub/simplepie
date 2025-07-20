9d2eef328289ea1e881f0eb549b10801d7924ecb
GloXhub <gloxsociete@gmail.com>
2025-07-21 00:05:26 +0200

    Update autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -1,86 +1,26 @@
 <?php
-
-// SPDX-FileCopyrightText: 2004-2023 Ryan Parman, Sam Sneddon, Ryan McCue
-// SPDX-License-Identifier: BSD-3-Clause
-
-/**
- * PSR-4 implementation for SimplePie.
- *
- * After registering this autoload function with SPL, the following line
- * would cause the function to attempt to load the \SimplePie\SimplePie class
- * from /src/SimplePie.php:
- *
- *      new \SimplePie\SimplePie();
- *
- * @param string $class The fully-qualified class name.
- * @return void
- */
 spl_autoload_register(function ($class) {
-
-    // project-specific namespace prefix
+    // Namespace prefix à gérer
     $prefix = 'SimplePie\\';
 
-    // base directory for the namespace prefix
+    // Base directory relative où se trouve SimplePie
     $base_dir = __DIR__ . '/src/';
 
-    // does the class use the namespace prefix?
+    // Vérifier si la classe commence par le préfixe
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
+        // Non, ignorer
         return;
     }
 
-    // get the relative class name
+    // Obtenir la partie relative de la classe
     $relative_class = substr($class, $len);
 
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
+    // Construire le chemin vers le fichier
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-    // if the file exists, require it
+    // Inclure si le fichier existe
     if (file_exists($file)) {
         require $file;
     }
 });
-
-// autoloader
-spl_autoload_register(array(new SimplePie_Autoloader(), 'autoload'));
-
-if (!class_exists('SimplePie'))
-{
-	exit('Autoloader not registered properly');
-}
-
-/**
- * Autoloader class
- */
-class SimplePie_Autoloader
-{
-	protected $path;
-
-	/**
-	 * Constructor
-	 */
-	public function __construct()
-	{
-		$this->path = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'library';
-	}
-
-	/**
-	 * Autoloader
-	 *
-	 * @param string $class The name of the class to attempt to load.
-	 */
-	public function autoload($class)
-	{
-		// Only load the class if it starts with "SimplePie"
-		if (strpos($class, 'SimplePie') !== 0)
-		{
-			return;
-		}
-
-		$filename = $this->path . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-		include $filename;
-	}
-}
